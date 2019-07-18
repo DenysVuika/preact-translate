e96ba5a61df1a212c45cbbd7387be0bdb943fbfd
Denys Vuika <denys.vuika@gmail.com>
2019-07-18 21:15:56 +0100

    simple formatted translation

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+import {
+  TranslateContext,
+  TranslateProvider
+} from '@denysvuika/preact-translate';
+import { useContext } from 'preact/hooks';
 import './style';
-import { TranslateProvider } from '@denysvuika/preact-translate';
-import MainComponent from './main';
+
+function MainComponent() {
+  const { setLang, t, lang } = useContext(TranslateContext);
+
+  return (
+    <div>
+      <div>Lang: {lang}</div>
+      <div>{t('title')}</div>
+      <div>{t('subtitle')}</div>
+      <div>Formatted: {t('hello', { name: 'Bob' })}</div>
+      <div>
+        <button onClick={() => setLang('en')}>EN</button>
+        <button onClick={() => setLang('ua')}>UA</button>
+      </div>
+    </div>
+  );
+}
 
 export default function App() {
   return (
