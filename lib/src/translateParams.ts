f466f8ffba82b58b6b277f27b8f3c3bd6ce79466
Marwane <mbenoukaiss@gmail.com>
2024-01-20 22:44:32 +0100

    Default TranslateParams type

--- lib/src/translateParams.ts
+++ lib/src/translateParams.ts
@@ -2,6 +2,6 @@ import { type JSX } from "preact";
 
 export type TranslateParam = string | number | JSX.Element;
 
-export interface TranslateParams<T extends TranslateParam> {
+export interface TranslateParams<T extends TranslateParam = string | number> {
   [key: string]: T;
 }
